49513b2273cd572382eb59f40b49fbc6bbc090c0
Sahansa1 <sahansaamarasinghe@gmail.com>
2024-02-19 15:25:31 +0530

    Revert "Detailed Screen"
    This reverts commit d68fed7758af5ddb14fc46a9112e336963585c79.

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/widgets/Movies.dart';
-import 'Models/ListMovies.dart';
-import 'Screens/homePage.dart';
+import 'homePage.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,41 +9,5 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return HomeScreen();
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Movies'),
-      ),
-      body: FutureBuilder<List<ListMovies>>(
-        future: fetchMovies(), // Call fetchMovies to get the list of movies
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            // If data is still loading, show a CircularProgressIndicator
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.hasError) {
-            // If there's an error, display the error message
-            return Center(
-              child: Text('Error: ${snapshot.error}'),
-            );
-          } else {
-            // If data is available, pass the snapshot to the Movies widget
-            return Movies(snapshot: snapshot);
-          }
-        },
-      ),
-    );
-  }
-
-  Future<List<ListMovies>> fetchMovies() async {
-    // Implement your code to fetch movies data
-    // This is just a placeholder function, replace it with your actual implementation
-    return [];
   }
 }
--- flutter_application_1/lib/widgets/Movies.dart
+++ flutter_application_1/lib/widgets/Movies.dart
@@ -1,32 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Constants.dart';
-import 'package:flutter_application_1/Models/ListMovies.dart';
-import 'package:flutter_application_1/Screens/DetailScreen.dart';
 
 class Movies extends StatelessWidget {
   const Movies({
-    Key? key,
-    required this.snapshot, // Change this to snapshot
-  }) : super(key: key);
+    super.key, required this.snapshot,
+  });
 
-  final AsyncSnapshot<List<ListMovies>> snapshot; // Change this to AsyncSnapshot<List<ListMovies>>
+
+  final AsyncSnapshot snapshot; 
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 300,
+      height: 200, 
       width: double.infinity,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         physics: const BouncingScrollPhysics(),
         itemCount: snapshot.data!.length,
         itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(snapshot.data![index].title), // Example of accessing movie title
+         return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+           
+            height:200,
+            width:200,
+            child: Image.network(
+              filterQuality: FilterQuality.high,
+              fit: BoxFit.cover,
+              '${Constants.imagePath}${snapshot.data![index].poster_path}'),
+         )
           );
         },
       ),
     );
   }
 }
+
