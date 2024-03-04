b7d00b8028c3c8716e26bf0864e065e562a4226a
Sahansa1 <sahansaamarasinghe@gmail.com>
2024-03-04 22:17:39 +0530

    Search by actor name, in movies (movie detailed page)

---
+++ flutter_application_1/lib/Screens/actors_detailed_screen.dart
@@ -0,0 +1,244 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Constants.dart';
+import 'package:flutter_application_1/Models/list_actors.dart';
+import 'package:flutter_application_1/Models/list_movies.dart';
+import 'package:flutter_application_1/Screens/movie_detailed_screen.dart';
+
+
+class ActorsDetailsScreen extends StatelessWidget {
+  final int id;
+  final ListActors actor;
+
+  const ActorsDetailsScreen({
+    Key? key,
+    required this.actor,
+    required this.id,
+  }) : super(key: key);
+
+  @override
+
+  /*
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(slivers: [
+        SliverAppBar.large(
+          leading: const BackButton(),
+
+          backgroundColor: Colors.amber,
+          expandedHeight:500,
+          pinned: true,
+          flexibleSpace: FlexibleSpaceBar(
+            title:Text(actor.name),
+            background: Image.network(
+              '${Constants.imagePath}${actor.profile_path}',
+              filterQuality: FilterQuality.high,
+              fit: BoxFit.cover,
+            ),
+          ), 
+        ), 
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(children: [
+              const Text('Overview'
+              ),
+              const SizedBox(height: 16),
+              Text(
+                actor.original_name,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 16),
+
+              SizedBox(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        border:Border.all(
+                          color: Colors.lightBlue, 
+                        ),
+                        borderRadius: BorderRadius.circular(10),  
+                      ),
+                      child: Row(children: [
+                        const Text('Release Date: '),
+                        //Text(movie.release_date),
+                      ],),
+                    ),
+
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        border:Border.all(
+                          color: Colors.lightBlue, 
+                          ),
+                        borderRadius: BorderRadius.circular(10),  
+                      ),
+                      child: Row(children: [
+                        const Text('Rating: '),
+                        const Icon( Icons.star,
+                        color: Colors.amber,
+                        ),
+                        //Text('${movie.vote_average.toStringAsFixed(1)}/10'),
+                      ],),
+                    ),
+
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        border:Border.all(
+                          color: Colors.lightBlue, 
+                          ),
+                        borderRadius: BorderRadius.circular(10),  
+                      ),
+                      child: Row(children: [
+                        const Text('Popularity: '),
+                        //Text(movie.popularity.toString()),
+                      ],),
+                    ),
+
+                  ],
+                ),
+              ),
+            ],),
+          ),
+        )   
+      ],
+      ),
+      
+    );
+  }*/
+
+  Widget build(BuildContext context) {
+  return Scaffold(
+    body: CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          leading: const BackButton(),
+          backgroundColor: Colors.amber,
+          expandedHeight: 500,
+          pinned: true,
+          flexibleSpace: FlexibleSpaceBar(
+            background: Image.network(
+              '${Constants.imagePath}${actor.profile_path}',
+              filterQuality: FilterQuality.high,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  actor.name,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+                SizedBox(height: 16),
+                Text(
+                  'Original Name: ${actor.original_name}',
+                ),
+                SizedBox(height: 8),
+                Text(
+                  'Popularity: ${actor.popularity.toString()}',
+                ),
+                SizedBox(height: 8),
+                if (actor.gender == 1)
+                  const Text(
+                    'Gender: Female',
+                  ),
+                if (actor.gender == 2)
+                  const Text(
+                    'Gender: Male',
+                  ),
+                
+                SizedBox(height: 16),
+                const Text(
+                  'Movies Done:',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: SizedBox(
+            height: 200, 
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: actor.known_for.length,
+              itemBuilder: (context, index) {
+                final movie = ListMovies(
+     
+                  id: actor.known_for[index]['id'],
+                  title: actor.known_for[index]['title'],
+                  backdrop_path: actor.known_for[index]['backdrop_path'], 
+                  original_title: '', 
+                  overview: actor.known_for[index]['overview'],
+                  poster_path: actor.known_for[index]['poster_path'],
+                  popularity: actor.known_for[index]['popularity'].toDouble(),
+                   release_date: actor.known_for[index]['release_date'], 
+                   vote_average: actor.known_for[index]['vote_average'].toDouble(),
+                  movieId: actor.known_for[index]['id'], 
+                );            
+                  
+                
+                
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => MovieDetailsScreen(movie: movie, id: id,),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 8),
+                    padding: EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.lightBlue),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Image.network(
+                            '${Constants.imagePath}${movie.poster_path}',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          movie.title,
+                          style: TextStyle(fontSize: 16),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+
+                
+              },
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+}
+
--- flutter_application_1/lib/widgets/search_by_actor.dart
+++ flutter_application_1/lib/widgets/search_by_actor.dart
@@ -2,6 +2,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/Models/list_actors.dart';
+import 'package:flutter_application_1/Screens/actors_detailed_screen.dart';
+
 import 'package:flutter_application_1/repttext.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_application_1/Constants.dart';
@@ -161,8 +163,27 @@ class _searchbarfun2State extends State<searchbarfun2> {
                             physics: const BouncingScrollPhysics(),
                             itemBuilder: (context, index) {
                               return GestureDetector(
+                                /*onTap: () {
+                                  ListActors actor = ListActors(
+                                    id: searchresult[index]['id'],
+                                    name: searchresult[index]['name'],
+                                    original_name: searchresult[index]['original_name'],
+                                    known_for_department: searchresult[index]['known_for_department'],
+                                    profile_path: searchresult[index]['profile_path'],
+                                    popularity: searchresult[index]['popularity'].toDouble(),
+                                    gender: searchresult[index]['gender'],
+                                    known_for: List<Map<String, dynamic>>.from(searchresult[index]['known_for']),
+                                  );
+*/
                                 onTap: () {
-                                  // Handle actor tap
+                                  ListActors actor = searchresult[index];
+
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => ActorsDetailsScreen(id: actor.id, actor: actor,),
+                                    ),
+                                  );
                                 },
                                 child: Container(
                                   margin: const EdgeInsets.only(top: 4, bottom: 4),
@@ -182,7 +203,7 @@ class _searchbarfun2State extends State<searchbarfun2> {
                                             image: NetworkImage(
                                               'https://image.tmdb.org/t/p/w500${searchresult[index].profile_path}',
                                             ),
-                                            fit: BoxFit.fill,
+                                            fit: BoxFit.scaleDown,
                                           ),
                                         ),
                                       ),
@@ -201,6 +222,7 @@ class _searchbarfun2State extends State<searchbarfun2> {
                                                   '${searchresult[index].name}',
                                                 ),
                                               ),
+                                              
                                               Container(
                                                 width: MediaQuery.of(context).size.width * 0.4,
                                                 height: 85,
